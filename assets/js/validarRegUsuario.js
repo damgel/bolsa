7b81b4e3caedf9084b7d75ade5f10745136ce831
damgel <damgel@outlook.com>
2013-09-23 12:31:50 -0600

    fallback con modernizer y jqueryUI agregado para el tag DATE

--- assets/js/validarRegUsuario.js
+++ assets/js/validarRegUsuario.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
             fecha_nac:
                     {
                         required: true,
-                        date:true
+                        date: true
                     },
             direccion:
                     {
@@ -59,6 +59,17 @@ $(document).ready(function() {
                         required: true
 
                     }
+        }
+    });
+    // fallback que detecta el uso del tag DATE en html5 y si no 
+    //es compatible con el navegador, implemente el datepicker de jqueryUI.css
+    Modernizr.load({
+        test: Modernizr.inputtypes.date,
+        nope: ['http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.7/jquery-ui.min.js', 'jquery-ui.css'],
+        complete: function() {
+            $('input[type=date]').datepicker({
+                dateFormat: 'yy-mm-dd'
+            });
         }
     });
 
