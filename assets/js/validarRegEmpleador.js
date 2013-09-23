725f0a8f77774f87b5328cc941d78c5687f1010d
damgel <damgel@outlook.com>
2013-09-23 12:31:22 -0600

    eliminadas dos funciones para agregar divs, el callback de html5 funciona mejor, agregado.

--- assets/js/validarRegEmpleador.js
+++ assets/js/validarRegEmpleador.js
@@ -44,13 +44,6 @@ $(document).ready(function() {
                         required: true
 
                     }
-        },
-        highlight: function(element) {
-            $(element).closest('.control-group').removeClass('success').addClass('error');
-        },
-        success: function(element) {
-            element.text('OK!').addClass('valid')
-                    .closest('.control-group').removeClass('error').addClass('success');
         }
     });
 
