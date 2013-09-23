fa53920ff51a411883264099262107cb787604cd
damgel <damgel@outlook.com>
2013-09-22 21:30:31 -0600

    funciones jquery mejoradas

--- assets/js/validarLogin.js
+++ assets/js/validarLogin.js
@@ -7,18 +7,13 @@ $(document).ready(function() {
             {
                 required: true,
                 email: true
+                
             },
             passwordm:
                     {
                 minlength: 8,
                 required:true    
                     }
-        }, highlight: function (element) {
-            $(element).closest('.control-group').removeClass('success').addClass('error');
-        },
-        success: function (element) {
-            element.text('OK!').addClass('valid')
-            .closest('.control-group').removeClass('error').addClass('success');
         }
     });
 
