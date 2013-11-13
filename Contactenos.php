44c2014d794a3294f7c4acdb4b71b443ecd3a4fa
gaby <monteshernandez6@hotmail.com>
2013-11-13 00:29:08 -0600

    edit

--- Contactenos.php
+++ Contactenos.php
@@ -21,6 +21,11 @@
 
     </head>
     <body>
+    
+    
+    
+
+
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -44,6 +49,43 @@
 
   
          <div id="contenedor" class="container">
+              <?php
+if(isset($_POST['email'])) {
+
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+
+$email_to = "monteshernandez6@hotmail.com";
+$email_subject = "Contacto desde el sitio web";
+
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['nombre']) ||
+!isset($_POST['telefono']) ||
+!isset($_POST['email']) ||
+!isset($_POST['mensaje'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
+$email_message .= "Telefono: " . $_POST['telefono'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Mensaje: " . $_POST['mensaje'] . "\n\n";
+$email_from=$_POST['email'];
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: <'.$email_from.'>'."\r\n".
+'Reply-To: <'.$email_from.'>'."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+?>
+
             <div class="panel panel-primary">
                 <div class="panel-heading">Contactenos</div>
                 <div class="panel-body">
@@ -60,7 +102,7 @@
                     <div class="form-group">
                         <label for="Telefono_Contacto" class="col-lg-3 control-label"> Telefono de Contacto </label>
                         <div class="col-lg-4">
-                            <input type="tel" name="telefono_contacto" class="form-control" required pattern=".{7,15}">
+                            <input type="tel" name="telefono" class="form-control" required pattern=".{7,15}">
                         </div>
                     </div>
 
@@ -72,13 +114,13 @@
                     </div>
                     
                     <div class="form-group">    
-                        <label for="Mensaje" class="col-lg-3 control-label">Mensaje</label>
+                        <label for="Mensaje"  class="col-lg-3 control-label">Mensaje</label>
                         <div class="col-lg-6">
-                            <textarea name="textarea" class="form-control col-lg-6" rows="10" > </textarea>
+                            <textarea name="mensaje" class="form-control col-lg-6" rows="10" > </textarea>
 
                                 </div>
                              </div>
-                    <center><button type="submit" class="btn btn-primary btn-lg">Enviar</button></center>
+                    <center><button type="submit" name="enviar" class="btn btn-primary btn-lg">Enviar</button></center>
 
                 </div>
             </form>
