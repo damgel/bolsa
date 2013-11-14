f728518366a1556f3cf9529c18392cc855731ce0
damgel <damgel@outlook.com>
2013-11-14 14:56:32 -0600

    modificaciones a la base de datos.

--- db/bolsa.sql
+++ db/bolsa.sql
@@ -1,4 +1,4 @@
-# SQL Manager Lite for MySQL 5.3.1.7
+# SQL Manager for MySQL 4.6.0.1
 # ---------------------------------------
 # Host     : localhost
 # Port     : 3306
@@ -12,6 +12,8 @@
 
 SET FOREIGN_KEY_CHECKS=0;
 
+DROP DATABASE IF EXISTS `bolsa`;
+
 CREATE DATABASE `bolsa`
     CHARACTER SET 'latin1'
     COLLATE 'latin1_swedish_ci';
@@ -19,116 +21,98 @@ CREATE DATABASE `bolsa`
 USE `bolsa`;
 
 #
-# Structure for the `ofertasempleo` table : 
+# Structure for the `empresa` table : 
 #
 
-CREATE TABLE `ofertasempleo` (
-  `cod_ofertas` INTEGER(11) NOT NULL AUTO_INCREMENT,
-  `titulo_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `descripcion_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `area_ofertas` INTEGER(11) DEFAULT NULL,
-  `cargo_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `vacantes_ofertas` INTEGER(11) DEFAULT NULL,
-  `tipodecontratacion_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `anoexp_ofetas` INTEGER(11) DEFAULT NULL,
-  `edad` INTEGER(11) DEFAULT NULL,
-  `genero_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `vehiculo_ofetas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `salariomax_ofertas` DECIMAL(30,0) DEFAULT NULL,
-  `salariomin_ofertas` DECIMAL(30,0) DEFAULT NULL,
-  `departamento_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `exp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `tituloen_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `nivel_ofertas` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  PRIMARY KEY USING BTREE (`cod_ofertas`) COMMENT ''
-)ENGINE=InnoDB
-AUTO_INCREMENT=1 CHARACTER SET 'latin1' COLLATE 'latin1_swedish_ci'
-COMMENT=''
-;
+DROP TABLE IF EXISTS `empresa`;
+
+CREATE TABLE `empresa` (
+  `cod_em` int(11) NOT NULL AUTO_INCREMENT,
+  `imagen_em` binary(1) DEFAULT NULL,
+  `nombre_em` varchar(20) DEFAULT NULL,
+  `password_em` varchar(20) DEFAULT NULL,
+  `email_em` varchar(20) DEFAULT NULL,
+  `telefono_em` varchar(20) DEFAULT NULL,
+  `url_em` varchar(20) DEFAULT NULL,
+  `textarea_em` varchar(20) DEFAULT NULL,
+  `actividad_em` varchar(20) DEFAULT NULL,
+  `departamento_em` varchar(20) DEFAULT NULL,
+  `direccion_em` varchar(20) DEFAULT NULL,
+  `activa_em` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`cod_em`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #
-# Structure for the `reg_empleador` table : 
+# Structure for the `ofertas` table : 
 #
 
-CREATE TABLE `reg_empleador` (
-  `cod_emp` INTEGER(11) NOT NULL AUTO_INCREMENT,
-  `nombre_emp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `password_emp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `confirm_password_emp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `email_emp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `telefono_emp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `url_emp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `textarea_emp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `actividad_empresa` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  PRIMARY KEY USING BTREE (`cod_emp`) COMMENT ''
-)ENGINE=InnoDB
-AUTO_INCREMENT=1 CHARACTER SET 'latin1' COLLATE 'latin1_swedish_ci'
-COMMENT=''
-;
+DROP TABLE IF EXISTS `ofertas`;
+
+CREATE TABLE `ofertas` (
+  `cod_of` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_em` int(11) DEFAULT NULL,
+  `titulo_of` varchar(20) DEFAULT NULL,
+  `descripcion_of` varchar(20) DEFAULT NULL,
+  `area_of` int(11) DEFAULT NULL,
+  `cargo_of` varchar(20) DEFAULT NULL,
+  `vacantes_of` int(11) DEFAULT NULL,
+  `contratacion_of` varchar(20) DEFAULT NULL,
+  `anoexp_ofetas` int(11) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL,
+  `genero_of` varchar(20) DEFAULT NULL,
+  `vehiculo_ofetas` varchar(20) DEFAULT NULL,
+  `salariomax_of` decimal(30,0) DEFAULT NULL,
+  `salariomin_of` decimal(30,0) DEFAULT NULL,
+  `departamento_of` varchar(20) DEFAULT NULL,
+  `exp` varchar(20) DEFAULT NULL,
+  `tituloen_of` varchar(20) DEFAULT NULL,
+  `nivel_of` varchar(20) DEFAULT NULL,
+  `disponible_of` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`cod_of`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #
-# Structure for the `perfilempleador` table : 
+# Structure for the `usuario` table : 
 #
 
-CREATE TABLE `perfilempleador` (
-  `cod_perfilemp` INTEGER(11) NOT NULL AUTO_INCREMENT,
-  `cod_emp` INTEGER(11) DEFAULT NULL,
-  `direc_perfilemp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `departamento_perfilemp` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `subirlog_perfilemp` BINARY(1) DEFAULT NULL,
-  PRIMARY KEY USING BTREE (`cod_perfilemp`) COMMENT '',
-   INDEX `cod_emp` USING BTREE (`cod_emp`) COMMENT '',
-  CONSTRAINT `perfilempleador_fk1` FOREIGN KEY (`cod_emp`) REFERENCES `reg_empleador` (`cod_emp`)
-)ENGINE=InnoDB
-AUTO_INCREMENT=1 CHARACTER SET 'latin1' COLLATE 'latin1_swedish_ci'
-COMMENT=''
-;
+DROP TABLE IF EXISTS `usuario`;
 
-#
-# Structure for the `reg_usuario` table : 
-#
-
-CREATE TABLE `reg_usuario` (
-  `cod_regu` INTEGER(11) NOT NULL AUTO_INCREMENT,
-  `nombre_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `apellido_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `fecha_nac_regu` DATE DEFAULT NULL,
-  `direccion_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `telefono_contacto_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `email_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `password_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `password2_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `genero_regu` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  PRIMARY KEY USING BTREE (`cod_regu`) COMMENT ''
-)ENGINE=InnoDB
-AUTO_INCREMENT=1 CHARACTER SET 'latin1' COLLATE 'latin1_swedish_ci'
-COMMENT=''
-;
+CREATE TABLE `usuario` (
+  `cod_u` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_u` varchar(20) DEFAULT NULL,
+  `apellido_u` varchar(20) DEFAULT NULL,
+  `fecha_nac_u` date DEFAULT NULL,
+  `direccion_u` varchar(20) DEFAULT NULL,
+  `telefono_u` varchar(20) DEFAULT NULL,
+  `email_u` varchar(20) DEFAULT NULL,
+  `password_u` varchar(20) DEFAULT NULL,
+  `sexo_u` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`cod_u`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #
 # Structure for the `perfilusuario` table : 
 #
 
+DROP TABLE IF EXISTS `perfilusuario`;
+
 CREATE TABLE `perfilusuario` (
-  `cod_u` INTEGER(11) NOT NULL AUTO_INCREMENT,
-  `cod_regu` INTEGER(11) DEFAULT NULL,
-  `nombrereferencia1_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `apellidoreferencia1_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `telreferencia1_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `nombrereferencia2_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `apellidoreferencia2_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `telreferencia2_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `nombrereferencia3_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `apellidoreferencia3_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `telreferencia3_u` CHAR(20) COLLATE latin1_swedish_ci DEFAULT NULL,
-  `subir_u` BINARY(1) DEFAULT NULL,
-  PRIMARY KEY USING BTREE (`cod_u`) COMMENT '',
-   INDEX `cod_regu` USING BTREE (`cod_regu`) COMMENT '',
-  CONSTRAINT `perfilusuario_fk1` FOREIGN KEY (`cod_regu`) REFERENCES `reg_usuario` (`cod_regu`)
-)ENGINE=InnoDB
-AUTO_INCREMENT=1 CHARACTER SET 'latin1' COLLATE 'latin1_swedish_ci'
-COMMENT=''
-;
+  `cod_u` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_regu` int(11) DEFAULT NULL,
+  `nombrereferencia1_u` varchar(20) DEFAULT NULL,
+  `apellidoreferencia1_u` varchar(20) DEFAULT NULL,
+  `telreferencia1_u` varchar(20) DEFAULT NULL,
+  `nombrereferencia2_u` varchar(20) DEFAULT NULL,
+  `apellidoreferencia2_u` varchar(20) DEFAULT NULL,
+  `telreferencia2_u` varchar(20) DEFAULT NULL,
+  `nombrereferencia3_u` varchar(20) DEFAULT NULL,
+  `apellidoreferencia3_u` varchar(20) DEFAULT NULL,
+  `telreferencia3_u` varchar(20) DEFAULT NULL,
+  `subir_u` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`cod_u`) USING BTREE,
+  KEY `cod_u` (`cod_u`) USING BTREE,
+  CONSTRAINT `perfilusuario_fk1` FOREIGN KEY (`cod_u`) REFERENCES `usuario` (`cod_u`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 
