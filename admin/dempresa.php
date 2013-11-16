9fd1af9505152ad3c632b9862859c19f864d820b
damgel <damgel@outlook.com>
2013-11-15 18:42:04 -0600

    activar y desactivar empresas.

---
+++ admin/dempresa.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Desactivando empresa</title>
+    </head>
+    <body>
+        <?php
+        echo "<script>alert('PRUEBA DE CONCEPTO')</script>";
+        include_once '../clases/db_connect.php';
+        $cod_em = (int) $_GET['id'];
+        foreach ($_POST AS $key => $value) {
+            $_POST[$key] = mysql_real_escape_string($value);
+        }
+        $sql = "UPDATE `empresa` SET  `activa_em` =  'N'   WHERE `cod_em` = '$cod_em' ";
+        mysql_query($sql) or die(mysql_error());
+        header("Location: http://localhost:8000/admin/panel.php"); /* Redirect browser */
+        ?>
+
+    </body>
+</html>
+
