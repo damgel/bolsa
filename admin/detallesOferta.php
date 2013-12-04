c2a1a0dba02751738343625179e82f9f8cad81db
damgel <damgel@outlook.com>
2013-12-04 11:50:40 -0600

    time ago implementado en las ofertas.

--- admin/detallesOferta.php
+++ admin/detallesOferta.php
@@ -9,11 +9,14 @@
         <title> Panel Administrativo </title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../assets/css/contenido.css">
         <link rel="stylesheet" href="../assets/css/bootstrap.css">
         <link rel="stylesheet" href="../assets/css/normalize.css">
-        <link rel="stylesheet" href="../assets/css/contenido.css">
+
 
         <script src="../assets/js/jquery-v1.10.2.js"></script>
+        <script src="../assets/js/momments.js"></script>
+        <script src="../assets/js/timeago.js"></script>
         <script src="../assets/js/bootstrap.min.js"></script>
         <script src="../assets/js/holder.js"></script>
         <style>
@@ -27,26 +30,39 @@
             {
                 border:solid 1px #CCC;
                 border-left: solid 8px #05a8ff;
-                display: block;
+
                 margin: 25px;
                 padding: 10px;
                 overflow: hidden;
-                width: 80%;
+                width: 95%;
 
             }
             .well
             {
                 border-left: solid 8px #28921f;
+
             }
 
             .disponible
             {
                 text-align: center;
-                width: 40%;
+                width: 100%;
             }
             .disponible a:hover
             {
                 text-decoration: none;
+            }
+            #wellinfo
+            {
+
+
+                padding-left: 5px;
+                padding-right: 5px;
+            }
+            #timeagos
+            {
+                padding: 5px;
+                margin: 5px;
             }
         </style>
 
@@ -83,39 +99,67 @@
                                     foreach ($row AS $key => $value) {
                                         $row[$key] = stripslashes($value);
                                     }
+                                    $cod_em = $row['cod_em'];
                                     ?>
 
                                     <div class="oferta-contenedor">
                                         <h4>Detalles de la Oferta #: <?php echo nl2br($row['cod_oferta']) ?></h4>
-                                        <h3><a><?php echo nl2br($row['titulo_of']) ?></a></h3>
+                                        <h3 class="h3"><a><?php echo nl2br($row['titulo_of']) ?></a></h3>
                                         <hr>
                                         <h4><?php echo nl2br($row['descripcion_of']) ?></h4><br>
-                                        <h6>Publicado por: <?php //echo nl2br($row2['nombre_em']) ?></h6><br>
+                                        <h5>
+                                            <?php
+                                            $result2 = mysql_query("SELECT nombre_em FROM `empresa` where cod_em=$cod_em") or trigger_error(mysql_error());
+                                            while ($row2 = mysql_fetch_array($result2)) {
+                                                $nombre_empresa = $row2['nombre_em'];
+                                            }
+                                            ?>Publicado por:<?php echo "<b>".$nombre_empresa."</b>" ?></h5>
+                                        <h4><?php $fecha_ago = ($row['fecha_of']) ?></h4>
+                                        <?php echo "<span id='timeagos' data-livestamp='$fecha_ago'></span><br>" ?>
 
-                                        <div class="well">
-                                            <h4>Area de la empresa:<?php echo nl2br($row['area_of']) ?></h4>
-                                            <h4>Cargo Solicitado:<?php echo nl2br($row['cargo_of']) ?></h4>
-                                            <h4>Vacantes Disponibles:<?php echo nl2br($row['vacantes_of']) ?></h4>
-                                            <h4>Contratacion: <?php echo nl2br($row['contratacion_of']) ?></h4>
-                                            <h4>Edad requerida: <?php echo nl2br($row['edad']) ?></h4>
-                                            <h4>Genero requerido: <?php echo nl2br($row['genero_of']) ?></h4>
-                                            <h4>Vehiculo: <?php echo nl2br($row['vehiculo_ofetas']) ?></h4>
-                                            <h4>Salario Maximo: $<?php echo nl2br($row['salariomax_of']) ?></h4>
-                                            <h4>Salario Minimo: $<?php echo nl2br($row['salariomin_of']) ?></h4>
-                                            <h4>Departamento: <?php echo nl2br($row['departamento_of']) ?></h4>
-                                            <h4>Experiencia requerida: <?php echo nl2br($row['experiencia_of']) ?></h4>
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <div class="well">
+                                                    <h4>Area de la empresa:<?php echo nl2br($row['area_of']) ?></h4>
+                                                    <h4>Cargo Solicitado:<?php echo nl2br($row['cargo_of']) ?></h4>
+                                                    <h4>Vacantes Disponibles:<?php echo nl2br($row['vacantes_of']) ?></h4>
+                                                    <h4>Contratacion: <?php echo nl2br($row['contratacion_of']) ?></h4>
+                                                    <h4>Edad requerida: <?php echo nl2br($row['edad']) ?></h4>
+                                                    <h4>Genero requerido: <?php echo nl2br($row['genero_of']) ?></h4>
+                                                    <h4>Vehiculo: <?php echo nl2br($row['vehiculo_ofetas']) ?></h4>
+                                                    <h4>Salario Maximo: $<?php echo nl2br($row['salariomax_of']) ?></h4>
+                                                    <h4>Salario Minimo: $<?php echo nl2br($row['salariomin_of']) ?></h4>
+                                                    <h4>Departamento: <?php echo nl2br($row['departamento_of']) ?></h4>
+                                                    <h4>Experiencia requerida: <?php echo nl2br($row['experiencia_of']) ?></h4>
 
-                                            <form action="">
-                                                <?PHP
-                                                if ($row['disponible_of'] == 1) {
-                                                    echo " <h5 class='disponible'><div class='alert alert-success'><a class='alert-link'>DISPONIBLE</a></div></h5>";
-                                                    echo "<input type='submit' class='btn btn-block btn-success' name='Aplicar' value='Aplicar a esta Oferta'>";
-                                                } else {
-                                                    echo " <h5 class='disponible'><div class='alert alert-danger'><a class='alert-link'> NO DISPONIBLE</a></div></h5>";
-                                                }
-                                                ?>
+                                                    <form method="POST" action="">
+                                                        <input type="hidden" name="cod_em" value="<?php echo nl2br($row['cod_em']) ?>">
+                                                        <input type="hidden" name="cod_oferta" value="<?php echo nl2br($row['cod_oferta']) ?>">
+                                                        <input type="hidden" name="cod_u" value="<?php echo nl2br($row['cod_oferta']) ?>">
 
-                                            </form>
+
+
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="well">
+                                                    VALIDA HASTA:<br><?php echo nl2br($row['fechaFin_of']) ?>
+
+                                                </div>
+                                                <div class="well" id="wellinfo">
+                                                    <?PHP
+                                                    if ($row['disponible_of'] == 1) {
+                                                        echo " <h5 class='disponible'><div class='alert alert-success'><a class='alert-link'>DISPONIBLE</a></div></h5>";
+                                                        echo "<input type='submit' class='btn btn-block btn-success' name='Aplicar' value='Aplicar a esta Oferta'>";
+                                                    } else {
+                                                        echo " <h5 class='disponible'><div class='alert alert-danger'><a class='alert-link'> NO DISPONIBLE</a></div></h5>";
+                                                    }
+                                                    ?>
+                                                </div>
+
+
+                                            </div>
                                         </div>
 
                                     </div>
