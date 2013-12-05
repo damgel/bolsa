d429173e4a084a1eee5be12fa607c080b07d36ee
gaby <monteshernandez6@hotmail.com>
2013-12-05 03:04:24 -0600

    edit

--- admin/aempresa.php
+++ admin/aempresa.php
@@ -19,12 +19,26 @@
         $getemailEM = "select email_em from  `empresa` WHERE `cod_em` = '$cod_em' ";
         $emailEmpresa;
         while ($row = mysql_fetch_array($getemailEM)) {
-            $emailEmpresa = $row['email_em'];
+           
+		    $emailEmpresa = $row['email_em'];
+			
+			
         }
 
         // LA VARIABLE CON EL EMAIL ES "$emailEmpresa"
+		
 
-        header("Location: http://localhost:8000/admin/panel.php"); /* CUANDO TODO ESTE OK, REDIRECCIONAR A EL PANEL */
+$titulo = 'Sobre la oferta';
+$mensaje = 'Su oferta fue aceptada \r\n Saludos'; 
+$cabeceras = 'From: ufg@ufg.com' . "\r\n" . 
+  'Reply-To: ufg@ufg.com' . "\r\n" . 
+ 'X-Mailer: PHP/' . phpversion();  
+ 
+mail($emailEmpresa, $titulo, $mensaje, $cabeceras);
+
+		
+
+        header("Location: http://localhost/bolsa/admin/panel.php"); /* CUANDO TODO ESTE OK, REDIRECCIONAR A EL PANEL */
         ?>
 
 
